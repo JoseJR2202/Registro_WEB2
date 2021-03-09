cfef58aafbbb05d52433fda25302ff98e01c9777
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-03-09 15:09:18 -0400

    Add files via upload

--- web22/src/helpers/Conexion.java
+++ web22/src/helpers/Conexion.java
@@ -3,7 +3,10 @@ package helpers;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class Conexion {
@@ -11,6 +14,8 @@ public class Conexion {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private Properties propiedades = new Properties();
+	private Statement stmt;
+	private ResultSet rs;
 	public Conexion() {
 		// TODO Auto-generated constructor stub
 		 try{
@@ -29,7 +34,7 @@ public class Conexion {
 		return DB;
 	}
 	
-	public void dbPrepareStatement(String query, Object[] obj) {
+	public void dbPrepareStatement(String query, Object... obj) {
 		try {
 			this.pstmt = this.conn.prepareStatement(query);
 			int i=0;
@@ -49,5 +54,31 @@ public class Conexion {
 				e.printStackTrace();
 			}
 		}
-	} 
+	}
+	
+	public ArrayList<String> dbStatement(String query ) { 
+		try {  
+			this.stmt = this.conn.createStatement(); 
+			this.rs = this.stmt.executeQuery(query);
+			ArrayList<String> datos_usuario=new ArrayList<String>();
+			while(rs.next()) {
+			for(int i=0;i<rs.getMetaData().getColumnCount();i++)
+				datos_usuario.add(rs.getString(++i));
+			}
+			if(datos_usuario.get(0)!=null)
+				return datos_usuario;
+			else
+				return null;
+		} catch (SQLException e) { 
+			e.printStackTrace(); 
+		}finally { 
+			try { 
+				this.stmt.close(); 
+				this.rs.close(); 
+			} catch (SQLException e) { 
+				e.printStackTrace(); 
+			} 
+		} 
+		return null; 
+	}
 }
