a1990c1fa6baf06484bffd815f31c5685ece2683
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-03-23 20:33:01 -0400

    Add files via upload

--- web22/src/control/Controlador_dashboard.java
+++ web22/src/control/Controlador_dashboard.java
@@ -20,8 +20,8 @@ public class Controlador_dashboard {
         	ArrayList<String> datos=conectar.dbStatement("Select * from registro where correo like '"+session.getAttribute("usuario").toString()+"' and pass like  '"+nueva_clave+"' ");
             if(datos.get(0).equals(session.getAttribute("usuario")))
             	return "{\"status\":\"200\",\"correo\":\""+datos.get(0)+"\","
-            		+ "\"nombre\":\""+datos.get(1)+"\",\"nacimiento\":"+datos.get(2)+"\","
-            		+  "\"edad\":"+datos.get(3)+"\",\"ubicacion\":"+datos.get(4)+"\","+
+            		+ "\"nombre\":\""+datos.get(1)+"\",\"nacimiento\":\""+datos.get(2)+"\","
+            		+  "\"edad\":\""+datos.get(3)+"\",\"ubicacion\":\""+datos.get(4)+"\","+
             		"\"pass\":\""+datos.get(5)+"\"}";
             else
             	return "{\"status\":\"500\"}";
